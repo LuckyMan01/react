a56c1d2ad07a4da71f9751e2cba4bf579138b8b1
Vova <Vova>
2020-10-21 22:11:47 +0300

    first commit

---
+++ react/js/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// let number = 5;
+// console.log(number);
+
+// var Name = 'ivan';
+// Name + number;
+// alert ('Hello');
+// const result = confirm('are you here?');
+// console.log(result);
+// const answer = prompt ('wam e 18', '18');
+// console.log(answer);
+// const answers = [];
+// answers[0] = prompt('Яке ваше імя?', '');
+// answers[2] = prompt('Яке ваше фамілія?', '');
+// answers[3] = prompt('Скільки вам років?', '');
+
+// console.log(typeof(answers));
+//  const category = 'toys';
+//  console.log(`https://someurel.com/${category}/5`);
+//  const user = 'Ivan';
+//  alert(`Prevet, ${user}`);
+// console.log('arr' + " - object");
+// console.log( 4 + +" 5");
+// let incr = 10,
+//     decr = 10;
+// ++incr;
+// --decr; 
+
+// console.log(++incr);
+// console.log(--decr);
+// console.log(2*4 == '8');
+// const isChecked = true,
+//       isClose = false;
+// console.log(isChecked && isClose);
+const numverOfFilms = 'Скільки фільмів ви переглянули ?';
